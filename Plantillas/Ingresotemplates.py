e46efaca3a9631b6d97d8b7e4c26e0b0d638f2e0
MEras <meras@baustro.fin.ec>
2022-06-07 10:33:04 -0500

    Carga de Print y siclo repetitivo para carga de plantillas

--- Plantillas/Ingresotemplates.py
+++ Plantillas/Ingresotemplates.py
@@ -11,94 +11,75 @@ from selenium.webdriver.chrome.options import Options
 class Ingreso122006(unittest. TestCase):
 
     def setUp(self):
-        self.driver = webdriver.Chrome(executable_path=r"D:\RepoBancoDeAustro\chromedriver.exe")
+        self.driver = webdriver.Chrome(executable_path=r"/media/qacore/data/drivers/chromedriver")
     
     def test(self):
         driver = self.driver
         driver.maximize_window()
-        driver.get("http://localhost:94/WEB3/ingreso.html")
+        #Logeo al sistema Fitbank
+        driver.get("http://10.16.5.94:8380/WEB3/ingreso.html")
         usuario = driver.find_element_by_id("usuario")
         usuario.send_keys("ue01000663")
         clave = driver.find_element_by_xpath("//*[@id='ingreso']/input[2]")
         clave.send_keys("hdkjhkjhkhkasda" + Keys.ENTER)
-        time.sleep(3)
+        time.sleep(1.5)
+        
+        #ingreso a la transacion a subir los templates...
         transaccion = driver.find_element_by_id("entorno-pt")
         transaccion.send_keys("122006" + Keys.ENTER)
-        time.sleep(3)
+        time.sleep(1.5)
 
+        #Con el siguiente archivo es donde localiza las plantillas a subir... 
         with open('templates.csv') as File:
             reader = csv.DictReader(File, delimiter=',')
             for fila in reader:
-                
-                sub = driver.find_element_by_id('c_C1Csubsistema_0')
-                #sub.clear()
-                time.sleep(2)
-                sub.send_keys(fila["sub"])
-                sub.send_keys(Keys.TAB)
-                
-                trans = driver.find_element_by_id('c_C5Ctransaccion_0')
-                #trans.clear()
-                time.sleep(5)
-                trans.send_keys(fila["trans"])
-                time.sleep(3)
-                trans.send_keys(Keys.ENTER)
-                time.sleep(5)
 
+               # Ingreso al subsitema y transacion...
+                time.sleep(1.5)
+                sub = driver.find_element_by_id('c_C1Csubsistema_0')
+                time.sleep(1.5)
+                sub.send_keys(fila["sub"])
+                time.sleep(1.5)
+                sub.send_keys(Keys.TAB)  
+                trans = driver.find_element_by_id('c_C5Ctransaccion_0')
+                time.sleep(1.5)
+                trans.send_keys(fila["trans"])
+                time.sleep(1.5)
+                trans.send_keys(Keys.ENTER)
+                time.sleep(1.5)
+
+                #Carga de la plantilla...
+                print('Espere mientras se carga el plantilla...')
                 plan = driver.find_element_by_name("plantilla_0")
-                #plan.clear()
-                time.sleep(3)
+                time.sleep(1.5)
                 plan.send_keys(fila["plan"])
-                time.sleep(3)
+                time.sleep(1.5)
+
 
                 teje = driver.find_element_by_id("c_f5tipo_0")
-                #teje.clear()
-                time.sleep(3)
+                teje.clear()
+                time.sleep(1.5)
                 teje.send_keys(fila["teje"])
-                time.sleep(3)
+                time.sleep(1.5)
 
                 refe =  driver.find_element_by_name("f6referencia")
-                time.sleep(3)
+                refe.clear()
+                time.sleep(1.5)
                 refe.send_keys(fila["refe"])
-                time.sleep(3)
+                time.sleep(1.5)
+                
+                teclear = ActionChains(driver)
+                teclear.send_keys(Keys.F12).perform()
+                time.sleep(4)
 
-                print('Plantilla cargada' + (plan))
-
-
-            teclear = ActionChains(driver)
-            teclear.send_keys(Keys.F12).perform()
-            time.sleep(5)
-
-            
-
-        transaccion = driver.find_element_by_id("entorno-pt")
-        transaccion.clear()
-        transaccion.send_keys("122006")
-        time.sleep(5)
-        transaccion.send_keys(Keys.ENTER)
-        time.sleep(5)
-        driver.close()
-
-
-
-
-
-        
-        # trans.send_keys("6059")
-        # trans.send_keys("webdrive" + Keys.ENTER)
-        # time.sleep(2)
-        # template = driver.find_element_by_name("plantilla_0").send_keys("/home/qacore/Escritorio/UnoAUnoPagoCheques/templates/046059-PAGO_CHEQUES_transFERIDOS.xls")
-        # time.sleep(2)
-        # template = driver.find_element_by_name("f5tipo_combobox").send_keys("GRUPOS")
-        # time.sleep(2)
-        # template = driver.find_element_by_name("f6referencia").send_keys("GRUPO")
-        # time.sleep(5)
-        # print ('Reporte template')
-        # #button = driver.find_element_by_id("c_fboton_0")
-        # teclear = ActionChains(driver)
-        # teclear.send_keys(Keys.F12).perform()
-        # time.sleep(5)
-        # driver.close()
-    
+                print({sub, trans, plan})
+                
+                teclear = ActionChains(driver)
+                teclear.send_keys(Keys.F2).perform()
+                time.sleep(1.5)
+                refresh = driver.find_element_by_xpath("/html/body/div[13]/div[2]/div/table/tr[2]/td[1]/button").click()
+                time.sleep(4)
+                driver.close()
 
 if __name__ == '__main__':
      unittest.main()
